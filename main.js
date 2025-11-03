7f0cf1218ade083c7855fa82d91ba37911e9df83
EvanCastner <ekc30@uakron.edu>
2025-11-03 23:56:49 +0000

    Add single 3D cube

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 import * as THREE from 'three';
+import { ThreeMFLoader } from 'three/examples/jsm/Addons.js';
+import { mx_fractal_noise_float } from 'three/src/nodes/TSL.js';
+import { cameraPosition } from 'three/tsl';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+// Template 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x0077ff });
-const cube = new THREE.Mesh(geometry, material);
+// Cube variables
+var geom = new THREE.BoxGeometry(10, 10, 10);
+var mat = new THREE.MeshBasicMaterial({color: "green"});
+var vube = new THREE.Mesh(geom, mat);
+
+// Create the cube
 scene.add(cube);
+camera.position.x = 2;
+camera.position.y = 1;
+camera.position.z = 15;
 
-camera.position.z = 3;
+// Light source and direction
+var light = new THREE.AmbientLight( 0x404040 );                      // soft white light
+var directionalLight = new THREE.DirectionalLight( 0xfffff, 0.7 );   //White ligth at 70%
+scene.add( directionalLight );
 
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+// movement
+var xSpeed = 0.0001;
+var ySpeed = 0.0001;
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+
 }
-animate();
