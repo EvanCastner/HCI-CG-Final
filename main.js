3fe1b8c215fb0b14f0633f4412487037117e4b8e
EvanCastner <ekc30@uakron.edu>
2025-11-11 22:43:09 +0000

    Fix camera.aspect

--- main.js
+++ main.js
@@ -297,7 +297,7 @@ const render = function() {
 
 // Hnadle the window size
 window.addEventListener('resize',() => {
-    camera.aspect = window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
